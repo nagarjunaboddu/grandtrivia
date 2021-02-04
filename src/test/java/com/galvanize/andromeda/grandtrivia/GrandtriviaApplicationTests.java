3174dd9b846b01006f1b729f1187628cf42582fd
nagarjunaboddu <nagarjuna.boddu@cognizant.com>
2021-02-03 19:53:51 -0500

    Separate data for main and tests and use sample data from site for main

--- src/test/java/com/galvanize/andromeda/grandtrivia/GrandtriviaApplicationTests.java
+++ src/test/java/com/galvanize/andromeda/grandtrivia/GrandtriviaApplicationTests.java
@@ -2,8 +2,10 @@ package com.galvanize.andromeda.grandtrivia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GrandtriviaApplicationTests {
 
 	@Test
--- src/test/java/com/galvanize/andromeda/grandtrivia/controllers/QuestionsControllerIntegrationTests.java
+++ src/test/java/com/galvanize/andromeda/grandtrivia/controllers/QuestionsControllerIntegrationTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -13,12 +15,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class QuestionsControllerIntegrationTests {
 
     @Autowired
     MockMvc mockMvc;
 
     @Test
+    @Sql({"/data-test.sql"})
     public void getQuestionsTest() throws Exception {
         mockMvc.perform(get("/api/v1/grandtrivia/questions"))
                 .andExpect(status().isOk())
@@ -27,7 +31,7 @@ public class QuestionsControllerIntegrationTests {
                 .andExpect(jsonPath("$.[0].question").value("What did Yankee Doodle stick in his cap?"))
                 .andExpect(jsonPath("$.[0].question_num").value(1))
                 .andExpect(jsonPath("$.[0].timestamp").value("2018-02-02 20:04:25"))
-                .andExpect(jsonPath("$.[0].answers.length()").value(1))
+                .andExpect(jsonPath("$.[0].answers.length()").value(3))
                 .andExpect(jsonPath("$.[0].answers.[0].text").value("Feather"))
                 .andExpect(jsonPath("$.[0].answers.[0].correct").value(true))
                 .andExpect(jsonPath("$.[0].answers.[0].choice").value("A"));
